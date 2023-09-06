8d04183fc4bdca680dbf9001a063a3ec89962854
RothschildsMa <unityidma@gmail.com>
2023-09-06 11:06:29 +0900

    a

---
+++ ntsedApp/src/main/java/com/nts/api/NtsedAppApplication.java
@@ -0,0 +1,13 @@
+package com.nts.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtsedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtsedAppApplication.class, args);
+	}
+
+}
---
+++ ntsedApp/src/test/java/com/nts/api/NtsedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtsedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
