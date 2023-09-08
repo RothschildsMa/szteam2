f4fb8a9c77044e6e1b5f3cebd82a70eb6c7b0cd0
zxydi1 <895356009@qq.com>
2023-09-08 14:51:31 +0800

    張

---
+++ ntsedApp/src/main/java/com/nts/api/dao/loginMapper.java
@@ -0,0 +1,8 @@
+package com.nts.api.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface loginMapper {
+
+}
---
+++ ntsedApp/src/main/java/com/nts/api/service/loginService.java
@@ -0,0 +1,15 @@
+package com.nts.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nts.api.dao.loginMapper;
+
+@Service
+public class loginService {
+
+	@Autowired
+	private loginMapper mapper ;
+	
+	
+}
