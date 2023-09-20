a5e9cea7c6fd762996c632388fc95443494eb1e3
zxydi1 <895356009@qq.com>
2023-09-20 10:40:01 +0800

    login

--- ntsedApp/src/main/java/com/nts/api/dao/loginMapper.java
+++
@@ -1,8 +0,0 @@
-package com.nts.api.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface loginMapper {
-
-}
---
+++ ntsedApp/src/main/java/com/nts/api/entity/User.java
@@ -0,0 +1,21 @@
+package com.nts.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	private String userId;
+	
+	private String password;
+
+}
